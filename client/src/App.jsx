e1f1051ab892df7c4ba5b87e219c259e05b21ecf
clairemuiru1 <muiruclaire@gmail.com>
2024-05-20 14:16:33 +0300

    "Started on header"

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Todo from './components/Todo'
+import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Todo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <>
+      <div className='header text-center'>
+        <h1>Today's List</h1>
+        <button className='btn btn-primary mt-2'>Create Task</button>
+      </div>
+      <div className='task-container'>
+
+      </div>
+    </>
+  )
+}
+
+export default Todo
