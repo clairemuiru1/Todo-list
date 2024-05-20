22b351fdffc37b4a4b2d8ba55c039e42bed61385
clairemuiru1 <muiruclaire@gmail.com>
2024-05-20 20:41:44 +0300

    "Creted Popup"

--- client/src/Modals/CreateTask.jsx
+++ client/src/Modals/CreateTask.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
-import { Button, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import React, { useState } from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
-function CreateTask() {
+function CreateTask({ modal, toggle, save }) {
+    const [taskName, setTaskName] = useState('')
+    const [description, setDescription] = useState('')
+
+    const handleChange = (e) => {
+        const { name, value } = e.target 
+
+        if (name === 'taskName') {
+            setTaskName(value)
+        } else {
+            setDescription(value)
+        }
+    }
+
+    const handleSave = () => {
+        let taskObj = {}
+        taskObj["Name"] = taskName
+        taskObj["Description"] = description
+        save(taskObj)
+    }
+
     return (
         <div>
-            <Modal isOpen={modal} toggle={toggle} {...args}>
-                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Create Task</ModalHeader>
                 <ModalBody>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
+                    <form>
+                        <div className='form-group'>
+                            <label>Task Name</label>
+                            <input type="text" className="form-control" value={taskName} onChange={handleChange} name='taskName' />
+                        </div>
+                        <div className='form-group'>
+                            <label>Description</label>
+                            <textarea rows="5" className='form-control' value={description} onChange={handleChange} name='description'></textarea>
+                        </div>
+                    </form>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={toggle}>
-                        Do Something
+                    <Button color="primary" onClick={handleSave}>
+                        Create
                     </Button>{' '}
                     <Button color="secondary" onClick={toggle}>
                         Cancel
